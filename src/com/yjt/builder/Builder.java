d84c7e4086337f8ef4be407ba5cdde6542d3dd60
YuanJingtao <948099144@qq.com>
2017-02-10 16:49:42 +0800

    设计模式 1.单例模式 2.建造者模式模式 提交

---
+++ src/com/yjt/builder/Builder.java
@@ -0,0 +1,40 @@
+package com.yjt.builder;
+
+
+import com.yjt.abstractfactory.send.MailSender;
+import com.yjt.abstractfactory.send.Sender;
+import com.yjt.abstractfactory.send.SmsSender;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Builder 建造者模式
+ * 建造者模式将很多功能集成到一个类里，这个类可以创造出比较复杂的东西。所以与工程模式的区别就是：工厂模式关注的是创建单个产品，
+ * 而建造者模式则关注创建符合对象，多个部分。因此，是选择工厂模式还是建造者模式，依实际情况而定。
+ * @author YuanJingtao
+ * @version 1.0
+ * @date 2017-02-10 16:42
+ */
+public class Builder {
+
+    private List<Sender> list = new ArrayList<>();
+
+    private void produceMailSender(int count){
+        for(int i=0; i<count; i++){
+            list.add(new MailSender());
+        }
+    }
+
+    private void produceSmsSender(int count){
+        for(int i=0; i<count; i++){
+            list.add(new SmsSender());
+        }
+    }
+
+    public static void main(String[] args) {
+        Builder builder = new Builder();
+        builder.produceMailSender(10);
+    }
+
+}
